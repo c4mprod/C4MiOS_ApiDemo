9d9caae1d2489aae1c18a9b612b575fe1a354d37
Damien Praca <damien.praca@c4mprod.com>
2012-06-26 15:00:02 +0200

    updated license

--- C4MProject/Source/MasterViewController.h
+++ C4MProject/Source/MasterViewController.h
@@ -1,10 +1,24 @@
-//
-//  MasterViewController.h
-//  C4MProject
-//
-//  Created by  on 29/03/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/*******************************************************************************
+ * This file is part of the C4MiOS_ApiDemo project.
+ * 
+ * Copyright (c) 2012 C4M PROD.
+ * 
+ * C4MiOS_ApiDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * C4MiOS_ApiDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C4MiOS_ApiDemo. If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ * Contributors:
+ * C4M PROD - initial API and implementation
+ ******************************************************************************/
 
 #import <UIKit/UIKit.h>
 
